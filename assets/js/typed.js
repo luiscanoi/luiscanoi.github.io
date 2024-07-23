0d744208da7ed8702913825ac4d13b9492339f05
Luis Cano Irigoyen <luis.cano.iri@gmail.com>
2024-07-23 03:20:09 -0600

    change speed?

--- assets/js/typed.js
+++ assets/js/typed.js
@@ -399,11 +399,11 @@
         strings: ["These are the default values...", "You know what you should do?", "Use your own!", "Have a great day!"],
         stringsElement: null,
         // typing speed
-        typeSpeed: 1000,
+        typeSpeed: 1,
         // time before typing starts
         startDelay: 0,
         // backspacing speed
-        backSpeed: 2000,
+        backSpeed: 0,
         // shuffle the strings
         shuffle: false,
         // time before backspacing
